d887d236092ee7131b6a786456b0b2f74a00ac9f
DerekCHL <43765355+DrkDL@users.noreply.github.com>
2022-01-22 13:01:22 -0500

    Update site8.php

--- www/site8.php
+++ www/site8.php
@@ -28,7 +28,7 @@
     echo $feetInMile;
     ?>
 
-    <h1>Class and Objects</h1>
+    <h1>Class Objects and Constructors</h1>
 
     <?php
       // create a book class
@@ -38,24 +38,24 @@
         var $title;
         var $author;
         var $pages;
+
+        function __construct($aName, $aTitle, $aAuthor, $aPages)
+        {
+          $this->name = $aName;
+          $this->title = $aTitle;
+          $this->author = $aAuthor;
+          $this->pages = $aPages;
+        }
+
       }
       // init
-      $book1 = new Book;
-      $book1->name = "Book 1";
-      $book1->title = "Harry Potter";
-      $book1->author = "JK Rowling";
-      $book1->pages = 400;
+      $book1 = new Book("Book 1", "Harry Potter", "JK Rowling", 400);
+      $book2 = new Book("Book 2", "Lord Of The Rings", "Tolkien", 700);
 
       echo "<h3>",$book1->name, "</h3> <br>";
       echo $book1->title, "<br>";
       echo $book1->author, "<br>";
       echo $book1->pages, "<br>";
-
-      $book2 = new Book;
-      $book2->name = "Book 2";
-      $book2->title = "Derek Is Awesome";
-      $book2->author = "Derek";
-      $book2->pages = 10000;
 
       echo "<h3>",$book2->name, "</h3> <br>";
       echo $book2->title, "<br>";
@@ -63,9 +63,51 @@
       echo $book2->pages, "<br>";
     ?>
 
-    <h1>Constructors</h1>
+    <h1>Object Functions</h1>
 
-    
+    <?php
+      class Student
+      {
+        var $name;
+        var $major;
+        var $gpa;
+
+        function __construct($name, $major, $gpa)
+        {
+          $this->name = $name;
+          $this->major = $major;
+          $this->gpa = $gpa;
+        }
+
+        function hasHonors()
+        {
+          if($this->gpa >= 3.5)
+          {
+            return "true <br>";
+          }
+
+          return "false <br>";
+        }
+      }
+
+      $student1 = new Student("Brandon", "Computer Eng", 3.9);
+      $student2 = new Student("Daniel", "Computer Science", 3.4);
+      $student3 = new Student("Celena", "Art", 3.3);
+
+      echo $student1->hasHonors();
+      echo $student2->hasHonors();
+      echo $student3->hasHonors();
+    ?>
+
+    <h1>Challenge: Anime class</h1>
+
+    <?php
+      // create a anime class that will take three varaibles: name, score(0-10), genre
+      // create a constructor that overwrites those three varaibles from the user inputs
+      // create a custom function for checking the score, whether it is bad, soso, or amazing
+      // output everything including all the variables and score checking results
+
+     ?>
 
   </body>
 </html>
